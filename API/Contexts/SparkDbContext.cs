040de711f7b8742099246a2519866f51fbe3d743
BluB3ry <davidraharja910@gmail.com>
2023-06-22 12:30:46 +0700

    Add Repo
    Add Repo

---
+++ API/Contexts/SparkDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using API.Models;
+using API.Utility;
+using System.Data;
+
+namespace API.Contexts;
+public class SparkDbContext : DbContext
+{
+    public SparkDbContext(DbContextOptions<SparkDbContext> options) : base(options)
+    {
+
+    }
+
+    public DbSet<Account> Accounts { get; set; }
+    public DbSet<Admin> Admins { get; set; }
+    public DbSet<EventForm> EventForms { get; set; }
+    public DbSet<Event> Events { get; set; }
+    public DbSet<Payment> Payments { get; set; }
+    public DbSet<User> Users { get; set; }
+
+
+}
